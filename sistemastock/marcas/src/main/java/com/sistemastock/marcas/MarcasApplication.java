af26b258c12663f4be3e69ab466e718e107df21e
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-28 13:11:51 -0400

    primer commit

---
+++ sistemastock/marcas/src/main/java/com/sistemastock/marcas/MarcasApplication.java
@@ -0,0 +1,13 @@
+package com.sistemastock.marcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcasApplication.class, args);
+	}
+
+}
